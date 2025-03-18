f40f136e8d37382283fe220c1271555db59a5678
Leticia <leticiasoulos@gmail.com>
2025-03-18 12:08:35 -0300

    agregue division y cree un nuevo array de objetos para las divisiones. quise crear footer pero lo elimine

--- js/main.js
+++ js/main.js
@@ -6,36 +6,41 @@ let categorias = [
     {id : "zap", nombre: "Sneakers"}
 ]
 
-let productos = [
-    {categoria: "pan", nombre: "Barrel fleece pants", precio: "$29", img: "../img/pan1.jpg"},
-    {categoria: "pan", nombre: "Wide-leg pants", precio: "$25", img: "../img/pan2.jpg"},
-    {categoria: "pan", nombre: "Linen-blend pants with elastic waistband", precio: "$19", img: "../img/pan3.jpg"},
-    {categoria: "pan", nombre: "Baggy parachute pants", precio: "$45", img: "../img/pan4.jpg"},
-    {categoria: "pan", nombre: "Pleated pants with buckles", precio: "$30", img: "../img/pan5.jpg"},
-    {categoria: "pan", nombre: "Fluid wide-leg textured pants with drawstring", precio: "$29", img: "../img/pan6.jpg"},
-
-
-    {categoria: "zap", nombre: "High-top sneakers with hidden wedge", precio: "$50", img: "../img/zap1.jpg"},
-    {categoria: "zap", nombre: "Skate sneakers", precio: "$36", img: "../img/zap2.jpg"},
-    {categoria: "zap", nombre: "Skate sneakers with XL laces", precio: "$36", img: "../img/zap3.jpg"},
-    {categoria: "zap", nombre: "Tech volume sneakers", precio: "$46", img: "../img/zap4.jpg"},
-    {categoria: "zap", nombre: "Retro sneakers with lace detail", precio: "$35", img: "../img/zap5.jpg"},
-    {categoria: "zap", nombre: "Skate sneakers with animal print", precio: "$60", img: "../img/zap6.jpg"},
-
-    {categoria: "rem", nombre: "Sabrina Carpenter short-sleeve printed t-shirt", precio: "$18", img: "../img/rem1.jpg"},
-    {categoria: "rem", nombre: "Cinnamorroll short-sleeve printed t-shirt", precio: "$15", img: "../img/rem2.jpg"},
-    {categoria: "rem", nombre: "Short-sleeve printed t-shirt", precio: "$16", img: "../img/rem3.jpg"},
-    {categoria: "rem", nombre: "Short-sleeve V-neck printed t-shirt", precio: "$16", img: "../img/rem4.jpg"},
-    {categoria: "rem", nombre: "Short-sleeve boxy heavy cotton t-shirt", precio: "$10", img: "../img/rem5.jpg"},
-    {categoria: "rem", nombre: "The Vampire Diaries short-sleeve t-shirt", precio: "$18", img: "../img/rem6.jpg"},
-
-    {categoria: "ves", nombre: "Lace dress", precio: "$40", img: "../img/ves1.jpg"},
-    {categoria: "ves", nombre: "Midi dress with straps", precio: "$78", img: "../img/ves2.jpg"},
-    {categoria: "ves", nombre: "Printed midi dress with straps", precio: "$53", img: "../img/ves3.jpg"},
-    {categoria: "ves", nombre: "Maxi dress with straps", precio: "$60", img: "../img/ves4.jpg"},
-    {categoria: "ves", nombre: "Mini dress with belt", precio: "$38", img: "../img/ves5.jpg"},
-    {categoria: "ves", nombre: "Ice bandeau maxi dress", precio: "$73", img: "../img/ves6.jpg"}
+let division = [
+    {id: "wm", nombre : "Women",},
+    {id: "mn", nombre : "Men"},
+    {id: "tn", nombre: "Teen"}
 ]
+
+let productos = [
+    {categoria: "pan", nombre: "Barrel fleece pants", precio: "$29", img: "../img/pan1.jpg", division: "wm"},
+    {categoria: "pan", nombre: "Wide-leg pants", precio: "$25", img: "../img/pan2.jpg", division: "wm"},
+    {categoria: "pan", nombre: "Linen-blend pants with elastic waistband", precio: "$19", img: "../img/pan3.jpg", division: "wm"},
+    {categoria: "pan", nombre: "Baggy parachute pants", precio: "$45", img: "../img/pan4.jpg", division: "wm"},
+    {categoria: "pan", nombre: "Pleated pants with buckles", precio: "$30", img: "../img/pan5.jpg", division: "wm"},
+    {categoria: "pan", nombre: "Fluid wide-leg textured pants with drawstring", precio: "$29", img: "../img/pan6.jpg", division: "wm"},
+
+    {categoria: "zap", nombre: "High-top sneakers with hidden wedge", precio: "$50", img: "../img/zap1.jpg", division: "wm"},
+    {categoria: "zap", nombre: "Skate sneakers", precio: "$36", img: "../img/zap2.jpg", division: "wm"},
+    {categoria: "zap", nombre: "Skate sneakers with XL laces", precio: "$36", img: "../img/zap3.jpg", division: "wm"},
+    {categoria: "zap", nombre: "Tech volume sneakers", precio: "$46", img: "../img/zap4.jpg", division: "wm"},
+    {categoria: "zap", nombre: "Retro sneakers with lace detail", precio: "$35", img: "../img/zap5.jpg", division: "wm"},
+    {categoria: "zap", nombre: "Skate sneakers with animal print", precio: "$60", img: "../img/zap6.jpg", division: "wm"},
+
+    {categoria: "rem", nombre: "Sabrina Carpenter short-sleeve printed t-shirt", precio: "$18", img: "../img/rem1.jpg", division: "wm"},
+    {categoria: "rem", nombre: "Cinnamorroll short-sleeve printed t-shirt", precio: "$15", img: "../img/rem2.jpg", division: "wm"},
+    {categoria: "rem", nombre: "Short-sleeve printed t-shirt", precio: "$16", img: "../img/rem3.jpg", division: "wm"},
+    {categoria: "rem", nombre: "Short-sleeve V-neck printed t-shirt", precio: "$16", img: "../img/rem4.jpg", division: "wm"},
+    {categoria: "rem", nombre: "Short-sleeve boxy heavy cotton t-shirt", precio: "$10", img: "../img/rem5.jpg", division: "wm"},
+    {categoria: "rem", nombre: "The Vampire Diaries short-sleeve t-shirt", precio: "$18", img: "../img/rem6.jpg", division: "wm"},
+
+    {categoria: "ves", nombre: "Lace dress", precio: "$40", img: "../img/ves1.jpg", division: "wm"},
+    {categoria: "ves", nombre: "Midi dress with straps", precio: "$78", img: "../img/ves2.jpg", division: "wm"},
+    {categoria: "ves", nombre: "Printed midi dress with straps", precio: "$53", img: "../img/ves3.jpg", division: "wm"},
+    {categoria: "ves", nombre: "Maxi dress with straps", precio: "$60", img: "../img/ves4.jpg", division: "wm"},
+    {categoria: "ves", nombre: "Mini dress with belt", precio: "$38", img: "../img/ves5.jpg", division: "wm"},
+    {categoria: "ves", nombre: "Ice bandeau maxi dress", precio: "$73", img: "../img/ves6.jpg", division: "wm"}
+];
 
 let carrito = [];
 
